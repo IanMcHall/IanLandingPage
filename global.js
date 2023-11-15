1ba468c9b74392f58e313745ba84980db163f17b
Ian <ianmchall@gmail.com>
2023-11-15 14:15:21 -0500

    Fixed hamburger menu, altered colors, expanded js

--- global.js
+++ global.js
@@ -1,15 +1,18 @@
 const ian = {
     firstName: "Ian",
     lastName: "Hall",
+    altName: "I. M. Hall",
     edu1: "Bachelor of Arts in English",
-    edu2: "Master of Fine Arts in Creative Writing"
+    edu1Short: "BA",
+    edu2: "Master of Fine Arts in Creative Writing",
+    edu2Short: "MFA"
 }
 
 const heading = document.getElementById("heading");
-heading.innerHTML = `${ian.firstName + ' ' + ian.lastName}`;
+heading.innerHTML = `${ian.altName}`;
 
 
-function myFunction() {
+function openMenu() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
