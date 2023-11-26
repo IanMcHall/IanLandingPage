9a6192c3fb3a04a2ed5501203c6b70e9d6470a06
Ian <ianmchall@gmail.com>
2023-11-26 01:21:29 -0500

    Began API implementation

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+
 const ian = {
-    firstName: "Ian",
-    lastName: "Hall",
-    altName: "I. M. Hall",
-    edu1: "Bachelor of Arts in English",
-    edu1Short: "BA",
-    edu2: "Master of Fine Arts in Creative Writing",
-    edu2Short: "MFA"
-}
+  firstName: "Ian",
+  lastName: "Hall",
+  altName: "I. M. Hall",
+  edu1: "Bachelor of Arts in English",
+  edu1Short: "BA",
+  edu2: "Master of Fine Arts in Creative Writing",
+  edu2Short: "MFA",
+};
 
 const heading = document.getElementById("heading");
 heading.innerHTML = `${ian.altName}`;
+
+window.onload = function () {
+  fetchQuote();
+};
+
+function fetchQuote() {
+  const url = "https://zenquotes.io/api/today";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      displayQuote(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching programming quote:", error);
+    });
+}
+
+function displayProgrammingQuote(data) {
+  document.getElementById("api_quote").innerText =
+    data.en + "\n- " + data.author;
+}
